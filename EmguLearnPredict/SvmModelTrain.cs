7770d095e74f53ab229d5400fd5e9e6fe96261df
KomarovDN <dkomgameplay@gmail.com>
2017-05-05 16:01:17 +0300

    EmguLearnPredict:     - Changed namespace.

--- EmguLearnPredict/SvmModelTrain.cs
+++ EmguLearnPredict/SvmModelTrain.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmguTestMachineLearningWithImages
+namespace EmguLearnPredict
 {
     public enum ModelType { SvmModel, KnModel };
 
